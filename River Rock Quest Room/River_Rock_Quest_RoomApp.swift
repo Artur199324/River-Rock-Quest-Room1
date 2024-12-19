4ffdc39ee4e637d7b197943f6184601473e13f2e
Artur199324 <artyr241993@gmail.com>
2024-12-19 15:38:27 +0200

    Initial Commit

---
+++ River Rock Quest Room/River_Rock_Quest_RoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  River_Rock_Quest_RoomApp.swift
+//  River Rock Quest Room
+//
+//  Created by Artur on 19.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct River_Rock_Quest_RoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
